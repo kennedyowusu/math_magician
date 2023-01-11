fe549efb1e69493104bb2f0de750cdb4953465fa
kennedyowusu <kennediowusu@gmail.com>
2023-01-11 18:59:10 +0000

    convert app.js to hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -22,147 +22,149 @@ const Calculator = () => {
   return (
     <div className="Calculator">
       <table>
-        <tr className="table-row">
-          <td colSpan="4">
-            <input
-              type="text"
-              name="display"
-              readOnly
-              className="outcome"
-              value={total || next || operation}
-            />
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <button type="button" onClick={handleClick}>
-              AC
-            </button>
-          </td>
-          <td>
-            <button type="button" onClick={handleClick}>
-              +/-
-            </button>
-          </td>
-          <td>
-            <button type="button" onClick={handleClick}>
-              %
-            </button>
-          </td>
-          <td>
-            <button
-              type="button"
-              onClick={handleClick}
-              className="btn-operator"
-            >
-              ÷
-            </button>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <button type="button" onClick={handleClick}>
-              7
-            </button>
-          </td>
-          <td>
-            <button type="button" onClick={handleClick}>
-              8
-            </button>
-          </td>
-          <td>
-            <button type="button" onClick={handleClick}>
-              9
-            </button>
-          </td>
-          <td>
-            <button
-              type="button"
-              onClick={handleClick}
-              className="btn-operator"
-            >
-              x
-            </button>
-          </td>
-        </tr>
+        <tbody>
+          <tr className="table-row">
+            <td colSpan="4">
+              <input
+                type="text"
+                name="display"
+                readOnly
+                className="outcome"
+                value={total || next || operation}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button type="button" onClick={handleClick}>
+                AC
+              </button>
+            </td>
+            <td>
+              <button type="button" onClick={handleClick}>
+                +/-
+              </button>
+            </td>
+            <td>
+              <button type="button" onClick={handleClick}>
+                %
+              </button>
+            </td>
+            <td>
+              <button
+                type="button"
+                onClick={handleClick}
+                className="btn-operator"
+              >
+                ÷
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button type="button" onClick={handleClick}>
+                7
+              </button>
+            </td>
+            <td>
+              <button type="button" onClick={handleClick}>
+                8
+              </button>
+            </td>
+            <td>
+              <button type="button" onClick={handleClick}>
+                9
+              </button>
+            </td>
+            <td>
+              <button
+                type="button"
+                onClick={handleClick}
+                className="btn-operator"
+              >
+                x
+              </button>
+            </td>
+          </tr>
 
-        <tr>
-          <td>
-            <button type="button" onClick={handleClick}>
-              4
-            </button>
-          </td>
-          <td>
-            <button type="button" onClick={handleClick}>
-              5
-            </button>
-          </td>
-          <td>
-            <button type="button" onClick={handleClick}>
-              6
-            </button>
-          </td>
-          <td>
-            <button
-              type="button"
-              onClick={handleClick}
-              className="btn-operator"
-            >
-              -
-            </button>
-          </td>
-        </tr>
+          <tr>
+            <td>
+              <button type="button" onClick={handleClick}>
+                4
+              </button>
+            </td>
+            <td>
+              <button type="button" onClick={handleClick}>
+                5
+              </button>
+            </td>
+            <td>
+              <button type="button" onClick={handleClick}>
+                6
+              </button>
+            </td>
+            <td>
+              <button
+                type="button"
+                onClick={handleClick}
+                className="btn-operator"
+              >
+                -
+              </button>
+            </td>
+          </tr>
 
-        <tr>
-          <td>
-            <button type="button" onClick={handleClick}>
-              1
-            </button>
-          </td>
-          <td>
-            <button type="button" onClick={handleClick}>
-              2
-            </button>
-          </td>
-          <td>
-            <button type="button" onClick={handleClick}>
-              3
-            </button>
-          </td>
-          <td>
-            <button
-              type="button"
-              onClick={handleClick}
-              className="btn-operator"
-            >
-              +
-            </button>
-          </td>
-        </tr>
+          <tr>
+            <td>
+              <button type="button" onClick={handleClick}>
+                1
+              </button>
+            </td>
+            <td>
+              <button type="button" onClick={handleClick}>
+                2
+              </button>
+            </td>
+            <td>
+              <button type="button" onClick={handleClick}>
+                3
+              </button>
+            </td>
+            <td>
+              <button
+                type="button"
+                onClick={handleClick}
+                className="btn-operator"
+              >
+                +
+              </button>
+            </td>
+          </tr>
 
-        <tr>
-          <td colSpan="2">
-            <button type="button" onClick={handleClick}>
-              0
-            </button>
-          </td>
+          <tr>
+            <td colSpan="2">
+              <button type="button" onClick={handleClick}>
+                0
+              </button>
+            </td>
 
-          <td>
-            <button type="button" onClick={handleClick}>
-              .
-            </button>
-          </td>
+            <td>
+              <button type="button" onClick={handleClick}>
+                .
+              </button>
+            </td>
 
-          <td>
-            <button
-              type="button"
-              onClick={handleClick}
-              className="btn-operator"
-            >
-              =
-            </button>
-          </td>
-        </tr>
+            <td>
+              <button
+                type="button"
+                onClick={handleClick}
+                className="btn-operator"
+              >
+                =
+              </button>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
